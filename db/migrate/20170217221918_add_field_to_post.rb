07224fa6a58cdecbd18cc0cc622633219630390d
Jerementor <jerementor@gmail.com>
2017-02-17 17:25:15 -0500

    added subtitle to post model

---
+++ db/migrate/20170217221918_add_field_to_post.rb
@@ -0,0 +1,5 @@
+class AddFieldToPost < ActiveRecord::Migration[5.0]
+  def change
+    add_column :posts, :subtitle, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170216071805) do
+ActiveRecord::Schema.define(version: 20170217221918) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170216071805) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "subtitle"
   end
 
 end
