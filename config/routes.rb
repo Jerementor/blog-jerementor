da0428fd2fe39859f2fab503be2f9617609a2d12
Jerementor <jerementor@gmail.com>
2017-02-18 22:39:42 -0500

    removed model from friendly id url

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+
+  devise_scope :user do
+    get "/admin" => "devise/sessions#new" # custom path to login/sign_in
+  end
+
+  # resources :posts
+  resources :posts, path: ''
+
   root 'posts#index'
+
+  # get ':title' => 'posts#show'
 end
